d25e2621385173f52ace31bb605a547e16cd871f
Sk-Mahammad-Asif <76578809+Sk-Mahammad-Asif@users.noreply.github.com>
2023-01-19 10:52:40 +0530

    Update productDB.js

--- productDB.js
+++ productDB.js
@@ -1,14 +1,14 @@
-
+//parsing data from local machine to mongodb for updating.
 require("dotenv").config();
-const connectDB = require("./db/connect");
-const Product = require("./models/product");
-const ProductJson = require("./products.json");
+const connectDB = require("./db/connect"); // setup connection.
+const Product = require("./models/product");//import models schema.
+const ProductJson = require("./products.json");//import data
 
 const start = async () => {
      try {
-        await connectDB(process.env.MONGODB_URL);
-        await Product.deleteMany();
-        await Product.create(ProductJson);
+        await connectDB(process.env.MONGODB_URL);//connect to mongodb
+        await Product.deleteMany();//if any update first delete previous schema and update latest schema.
+        await Product.create(ProductJson);//pass the data
         console.log("success");
      }catch (error){
         console.log(error);
