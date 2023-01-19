559300da882bf27dfc4a0c740bf64163f3539f4f
Sk-Mahammad-Asif <76578809+Sk-Mahammad-Asif@users.noreply.github.com>
2023-01-19 10:28:53 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 //const uri = "mongodb+srv://Asif:CBPzxeokV6KB8dvr@asifapi.l3a9ieo.mongodb.net/AsifAPI?retryWrites=true&w=majority";
 
+//recive argument uri(same like props) from app.js file 
 const connectDB = (uri) => {
     console.log("connect db");
     return mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
