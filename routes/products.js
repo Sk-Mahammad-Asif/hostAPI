3d925783415c8e697197ff4fe9e5adb0db6c3c92
Sk-Mahammad-Asif <76578809+Sk-Mahammad-Asif@users.noreply.github.com>
2023-01-19 10:20:02 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-
+//getting this two getAllProducts, getAllProductsTesting from here define ../controllers/products
 const {getAllProducts, getAllProductsTesting,} = require("../controllers/products");
 
 //define routes
-//for fethcing data home page
+
+//for fethcing data home page actual data show from here browser.
 router.route("/").get(getAllProducts);
+
 //for testing purpose use POSTMAN API Testing.
 router.route("/testing").get(getAllProductsTesting);
 
